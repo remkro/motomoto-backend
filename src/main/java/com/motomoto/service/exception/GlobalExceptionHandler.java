476bdd1dccb2bcc76761c84b3b963ce2b8fd2e8f
remkro <krolak.remek@gmail.com>
2025-07-20 20:37:52 +0200

    Feature - handle all other exceptions

--- src/main/java/com/motomoto/service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/motomoto/service/exception/GlobalExceptionHandler.java
@@ -1,25 +1,45 @@
 package com.motomoto.service.exception;
 
 import com.motomoto.controller.dto.AppExceptionDto;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
-
 import static com.motomoto.service.exception.ExceptionCategory.BUSINESS;
+import static com.motomoto.service.exception.ExceptionCategory.TECHNICAL;
+import static com.motomoto.service.exception.ExceptionErrorCode.somethingWentWrong;
+import static java.time.LocalDateTime.now;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<AppExceptionDto> handleUserAlreadyExists(UserAlreadyExistsException e) {
+        log.error("Exception: {} at: {}", ExceptionUtils.getStackTrace(e), now());
+
         AppExceptionDto error = new AppExceptionDto(
                 e.errorCode().getShortName(),
                 e.getMessage(),
                 BUSINESS.getShortName(),
-                LocalDateTime.now().toString()
+                now().toString()
         );
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<AppExceptionDto> handleOtherException(Exception e) {
+        log.error("Exception: {} at: {}", ExceptionUtils.getStackTrace(e), now());
+
+        return ResponseEntity
+                .internalServerError()
+                .body(new AppExceptionDto(
+                        somethingWentWrong().getShortName(),
+                        "Something went wrong",
+                        TECHNICAL.getShortName(),
+                        now().toString())
+                );
     }
 }
